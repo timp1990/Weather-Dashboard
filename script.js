140aca454a66c53a6748422edb45fdfebb94e750
Tim Polo <timothyjpolo@gmail.com>
2022-03-26 16:56:09 +1100

    First Working Draft

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ var searchInput = document.getElementById('search-input');
 var previousSearchesDiv = document.getElementById('previous-searches');
 var cityResultsTodayDiv = document.getElementById('city-today-div');
 var fiveDayForecastDiv = document.getElementById('five-day-forecast-div');
+var fiveDayHeaderDiv = document.getElementById('five-day-header-div');
 var previousSearchesUL = document.getElementById('previous-searches-UL');
 var clearPreviousSearchesButton = document.getElementById('clear-previous-searches');
 var cityTimeHeader = document.getElementById('city-time-header');
@@ -12,7 +13,6 @@ var currentTemp = document.getElementById('current-temp');
 var currentWind = document.getElementById('current-wind');
 var currentHumidity = document.getElementById('current-humidity');
 var currentUV = document.getElementById('current-uv');
-
 
 // global variables
 var openWeatherAPIKey = 'a667932ac6ff632247e4626d841090bd';
@@ -27,9 +27,10 @@ var city = '';
 function subitFormHandler(event) {
     event.preventDefault();
     if (searchInput.value) {
+        pageEmpty();
         city = searchInput.value;
         getGeoCodingInfo(city);
-        searchInput.textContent = '';
+        searchInput.value = '';
 
     } else {
         alert('Please enter a valid city');
@@ -37,7 +38,6 @@ function subitFormHandler(event) {
 }
 
 function getGeoCodingInfo(city) {
-    // http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
     // http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid={API key}
     var apiURL = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + openWeatherAPIKey;
 
@@ -48,7 +48,6 @@ function getGeoCodingInfo(city) {
                     .then(function (data) {
                         storeCityData(data);
                         getCurrentWeather(cityGeoData)
-                        getFiveDayForecast(cityGeoData)
                         displayPreviousSearches()
 
                     })
@@ -91,51 +90,61 @@ function getCurrentWeather(cityGeoData) {
         .catch(function (error) {
             alert('Unable to connect to Open Weather');
         })
-
-    return;
 }
 
 function displayCurrentWeather(data) {
     console.log(data)
     // Current Weather
     cityTimeHeader.textContent = cityGeoData.name + ' : ' + date;
-    currentTemp.textContent = 'Temp : ' + data.current.temp;
-    currentWind.textContent = 'Wind : ' + data.current.wind_speed;
-    currentHumidity.textContent = 'Humidity : ' + data.current.humidity;
+    currentTemp.textContent = 'Temp : ' + data.current.temp + " C";
+    currentWind.textContent = 'Wind : ' + data.current.wind_speed + " m/s";
+    currentHumidity.textContent = 'Humidity : ' + data.current.humidity + " %";
     currentUV.textContent = 'UV Index : ' + data.current.uvi;
+    var UVSeverityClass = '';
+    if (data.current.uvi < 3) {
+        UVSeverityClass = 'btn btn-success';
+    } else if (data.current.uvi <= 7) {
+        UVSeverityClass = 'btn btn-warning';
+    } else {
+        UVSeverityClass = 'btn btn-danger';
+    }
+    currentUV.setAttribute('class', UVSeverityClass);
 
     // 5 day forecast
+    var fiveDayHeader = document.createElement('h3');
+    fiveDayHeader.textContent = "Five Day Forecast : ";
+    fiveDayHeaderDiv.appendChild(fiveDayHeader)
+
     for (var i = 0; i < 5; i++) {
         // Get Forecast Variables
-        var forecastDay = parseInt(currentDay) + i;
+        var forecastDay = parseInt(currentDay) + i + 1;
+        var iconID = data.daily[i].weather[0].icon;
+        var iconURL = "http://openweathermap.org/img/wn/" + iconID + "@2x.png";
 
         // Add each day card
         var thisCardDiv = document.createElement('div');
-        thisCardDiv.setAttribute('style', 'card w-15');
+        thisCardDiv.setAttribute('class', 'card m-1');
         var thisCardDivHeader = document.createElement('h4');
         thisCardDivHeader.textContent = forecastDay + "/" + currentMonthAndYear;
+        var iconImg = document.createElement('img');
+        iconImg.setAttribute('src', iconURL);
 
 
-
-
+        var thisCardText = document.createElement('div');
+        thisCardText.innerHTML = "<p> Temp : " + data.daily[i].temp.day
+            + " C </p> <p> Wind : " + data.daily[i].wind_speed
+            + " m/s </p> <p> Humidity : " + data.daily[i].humidity
+            + " % </p>";
 
         // Append Children
         thisCardDiv.appendChild(thisCardDivHeader);
+        thisCardDiv.appendChild(iconImg);
+        thisCardDiv.appendChild(thisCardText);
         fiveDayForecastDiv.appendChild(thisCardDiv);
-
-
-
     }
-
-
 }
-
 
 // ----------------------------------------------------------------------------------------------------------------------------------
-
-function getFiveDayForecast(geoCode) {
-    return;
-}
 
 function displayPreviousSearches(previousSearchesString) {
     previousSearchesUL.innerHTML = '';
@@ -146,9 +155,15 @@ function displayPreviousSearches(previousSearchesString) {
     for (var i in previousSearchesListObject) {
         var thisListObjectLI = document.createElement('li');
         thisListObjectLI.textContent = i;
-        thisListObjectLI.setAttribute('style', 'panel panel-default');
+        thisListObjectLI.setAttribute('class', 'btn btn-primary w-100 mb-1');
         previousSearchesUL.appendChild(thisListObjectLI)
     }
+}
+
+function getPreviousCity(event) {
+    city = event.target.textContent;
+    getGeoCodingInfo(city);
+    searchInput.value = '';
 
 }
 
@@ -158,10 +173,15 @@ function clearSearches() {
     location.reload();
 }
 
+function pageEmpty() {
+    fiveDayHeaderDiv.innerHTML = '';
+    fiveDayForecastDiv.innerHTML = '';
+}
 
 function init() {
     // Add event handlers
     searchForm.addEventListener('submit', subitFormHandler);
+    previousSearchesUL.addEventListener('click', getPreviousCity);
     clearPreviousSearchesButton.addEventListener('click', clearSearches);
 
     if (localStorage.getItem('previousSearches')) {
